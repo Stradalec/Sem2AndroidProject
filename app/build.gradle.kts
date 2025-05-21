13cefa790b700153beb56b513d13b4781bd3ee5f
Stradalec <pochtatipo6@gmail.com>
2025-05-21 12:55:43 +0700

    design changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.androidplot:androidplot-core:1.5.10")
     implementation ("androidx.work:work-runtime-ktx:2.8.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
